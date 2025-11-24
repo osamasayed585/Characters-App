24809f48194098881037bf811dd73186ff61ae04
Usama Sayed <osamasayed585@gmail.com>
2025-11-25 01:30:39 +0200

    Refactor: Update package names in instrumentation tests
    This commit updates the expected package names in the instrumentation tests across the `app`, `details`, and `home` modules. This change aligns the test assertions with the recent refactoring of the project's package structure from `com.yassir` to `com.droidos`.
    
    **Key Changes:**
    
    *   **App Module:** In `ExampleInstrumentedTest.kt`, the expected package name is changed from `com.yassir.RMC` to `com.droidos.characters`.
    *   **Details Module:** In `ExampleInstrumentedTest.kt`, the expected package name is updated from `com.yassir.details.test` to `com.droidos.details.test`.
    *   **Home Module:** In `ExampleInstrumentedTest.kt`, the expected package name is updated from `com.yassir.home.test` to `com.droidos.home.test`.

--- app/src/androidTest/java/com/droidos/characters/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/droidos/characters/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.yassir.RMC", appContext.packageName)
+        assertEquals("com.droidos.characters", appContext.packageName)
     }
 }
--- feature/home/src/androidTest/java/com/droidos/home/ExampleInstrumentedTest.kt
+++ feature/home/src/androidTest/java/com/droidos/home/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.yassir.home.test", appContext.packageName)
+        assertEquals("com.droidos.home.test", appContext.packageName)
     }
 }
